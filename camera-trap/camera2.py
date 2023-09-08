f4c3d5706a6b43128bdc2532f21c7312eb5a7def
Lorna Mugambi <74656615+lornamugambi@users.noreply.github.com>
2023-09-08 10:58:04 +0300

    Update camera2.py

--- camera-trap/camera2.py
+++ camera-trap/camera2.py
@@ -1,15 +1,19 @@
 from picamera2 import Picamera2
 import time
+from datetime import datetime
 
 camera = Picamera2()
 #camera.rotation = 180
+
+
 #several images at once
-camera_config = camera.create_still_configuration(main={"size": (1920, 1080)}, lores={"size": (640, 480)}, display="lores")
-camera.configure(camera_config)
+camera_config = camera.create_still_configuration(main={"size": (1920, 1080)}, lores={"size>camera.configure(camera_config)
 
 camera.start_preview()
 camera.start()
 for i in range(5):
-    time.sleep(5)
-    camera.capture_file('/home/pi/Desktop/image%s.jpg' % i)
+    timestamp = datetime.now().strftime('%Y-%m-%d_%H-%M-%S')
+    filename = f'{timestamp}.jpg'
+    time.sleep(2)
+    camera.capture_file('/home/pi/Desktop/' + filename)
 camera.stop_preview()
