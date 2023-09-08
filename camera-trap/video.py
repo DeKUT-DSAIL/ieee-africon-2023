010c3fe48fc33c80b0e670590788022bd65985bb
Lorna Mugambi <74656615+lornamugambi@users.noreply.github.com>
2023-09-08 10:57:34 +0300

    Update video.py

--- camera-trap/video.py
+++ camera-trap/video.py
@@ -1,18 +1,21 @@
 from picamera2.encoders import  H264Encoder
 from picamera2 import Picamera2, Preview
 import time
+from datetime import datetime
 
 video = Picamera2()
 
-video_config = video.create_video_configuration(main={"size":(1920, 1080)}, lores={"size":(640,480)}, display="lores")
+timestamp = datetime.now().strftime('%Y-%m-%d_%H-%M-%S')
+filename = f'{timestamp}.h264'
+
+video_config = video.create_video_configuration(main={"size":(1920, 1080)}, lores={"size":(>
 
 video.configure(video_config)
 encoder = H264Encoder(bitrate=10000000)
 
-output = '/home/pi/Desktop/video.h264'
+output = '/home/pi/Desktop/' + filename
 video.start_preview()
 video.start_recording(encoder, output)
 time.sleep(10)
 video.stop_recording()
 video.stop_preview()
-
